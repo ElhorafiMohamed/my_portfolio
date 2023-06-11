2161c6e687b8c5a85f6740887a40022eabb492d5
Mohamed elhorafi <m.elhorafi@copag.ma>
2023-06-11 17:08:36 +0100

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactmeComponent } from './contactme/contactme.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import {NgxTreeGraphModule} from "ngx-tree-graph";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactmeComponent,
+    PortfolioComponent,
+    AboutComponent,
+    HomeComponent,
+    HeaderComponent,
+
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterOutlet,
+        AppRoutingModule,
+        NgxTreeGraphModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
